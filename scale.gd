cc885f5abd420de9cd02c3ebda3b8b8394abd588
jhache <julien.hache7@gmail.com>
2025-04-17 21:20:07 +0200

    feat: First attempt to add rotation to the stick.

--- scale.gd
+++ scale.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var SPEED = 300
+@export var SPEED = 50
+@export var ROTATION_SPEED = PI/6
 
 var dragging = false
 var of: Vector2
@@ -14,6 +15,14 @@ func _process(delta):
 		#  1: separate of in 2 variables: its length * its direction (.length and .normalize)
 		#  2: clamp its length to the desire values
 		velocity = of * delta * SPEED
+
+		# calculate the angle from the current position & rotation to the mouse position
+		var angle = global_position.rotated(rotation).angle_to_point(get_global_mouse_position())
+		# wrap the angle along the y axis, in order to revert the rotation
+		angle = wrap(angle, -1.5708, 1.5708)
+		# todo: limit the maximum rotation
+		rotation += angle * delta * ROTATION_SPEED
+
 		move_and_slide()
 
 
@@ -23,4 +32,3 @@ func _on_button_button_up() -> void:
 
 func _on_button_button_down() -> void:
 	dragging = true
-	#of = get_global_mouse_position() - global_position
