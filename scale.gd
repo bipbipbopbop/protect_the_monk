d59a8d4947697418e357053180b91c776cc66015
jhache <julien.hache7@gmail.com>
2025-04-17 20:48:56 +0200

    feat: Add speed to the stick drag.

--- scale.gd
+++ scale.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
-@export var SPEED = 500
+@export var SPEED = 300
 
 var dragging = false
-var of = Vector2(0, 0)
+var of: Vector2
 
 func _process(delta):
 	if dragging:
-		position = get_global_mouse_position() - of
+		# calculate the distance between self and the mouse positions
+		of = get_global_mouse_position() - global_position
+
+		# todo: If we want to limit the speed of this object, we need to:
+		#  1: separate of in 2 variables: its length * its direction (.length and .normalize)
+		#  2: clamp its length to the desire values
+		velocity = of * delta * SPEED
+		move_and_slide()
 
 
 func _on_button_button_up() -> void:
@@ -16,4 +23,4 @@ func _on_button_button_up() -> void:
 
 func _on_button_button_down() -> void:
 	dragging = true
-	of = get_global_mouse_position() - global_position
+	#of = get_global_mouse_position() - global_position
